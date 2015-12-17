c94036a4adbc8cf35c2393afc802854ca7814673
Ben Allred <skuttleman@gmail.com>
2015-12-16 20:53:27 -0700

    refactored

--- src/script/main.js
+++ src/script/main.js
@@ -7,17 +7,18 @@ function animateLetter(id, spacing) {
   var $letter = $('.num-' + id);
   var $sibling = $letter.clone();
   $letter.parent().append($sibling);
-  $letter.animate({ top: -spacing}, function() {
+  $letter.animate({ top: -spacing }, function() {
     $letter.remove();
   });
-  $sibling.animate({ top: -spacing}, function() {
+  $sibling.animate({ top: -spacing }, function() {
     $sibling.css({ top: 0 });
   });
 }
 
 function animateLetters(spacing, next) {
   var elapsed = next();
-  [1,2,4,8,16,32,64,128,256].forEach(function(id) {
-    if (elapsed % id === 0) animateLetter(id, spacing);
+  [0,1,2,3,4,5,6,7,8].forEach(function(_, i) {
+    id = Math.pow(3, i);
+    if (elapsed % id === 0) animateLetter(i, spacing);
   });
 }
